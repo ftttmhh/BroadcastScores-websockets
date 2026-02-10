5de6b4da2ccc53775cc204413ccc04aa92c93502
ftttmhh <firdousfatimah27@gmail.com>
2026-02-11 00:02:59 +0530

    implemented GET and POST routes for matches

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import pg from 'pg';
+import { Pool } from 'pg';
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined');
